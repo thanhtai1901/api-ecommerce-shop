fdc2f556d6bcf01075bed574fc47b14cf1392393
Tài Võ Thành <nhoxdo8@gmail.com>
2024-10-25 21:20:26 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express,{Request,Response} from "express";
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+
+app.get('/',(req:Request,res:Response)=>{
+    res.send('taideptrai1901')
+})
+
+app.listen(port,()=>{
+    console.log('hello tai dep trai number 1');
+    
+})
