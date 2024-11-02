59515c5583a9677c40a147fa71143923d714c83d
Tài Võ Thành <nhoxdo8@gmail.com>
2024-11-02 23:37:06 +0700

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import connectDatabase from './config/database/database.config';
+import usersRouter from './api/users/router/users.router';
 dotenv.config();
 const app = express();
 const PORT = 3000;
@@ -9,7 +10,7 @@ connectDatabase();
 app.get('/', (req: Request, res: Response) => {
   res.send('taideptrai1901');
 });
-
+app.use('/api/v1/users', usersRouter);
 app.listen(PORT, () => {
   console.log(`Server đang chạy ${PORT}`);
 });
